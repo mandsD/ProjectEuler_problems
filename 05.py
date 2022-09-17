c17c7a11688cdf7494adb154b1f907b2bc188b5d
mandsd <105010000+mandsd@users.noreply.github.com>
2022-09-17 19:54:46 -0300

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,14 @@
+""" 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? """
+
+import math
+def calculo():
+	ans = 1
+	for i in range(1, 21):
+		ans *= i // math.gcd(i, ans)
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(calculo())
